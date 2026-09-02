eb3d60ab379f5ae3aea3701881a4f7f80ae3745c
shreyanshisgoated12-coder <shreyanshisgoated12@gmail.com>
2026-09-02 16:02:38 +0530

    added manual runnnig agent instead of the two line text since it didnt look cool

--- agent.py
+++ agent.py
@@ -33,6 +33,37 @@ def run_agent(user_request: str):
     response = chat.send_message(user_request)
     print(response.text)
 
+def run_agent_manual(user_request: str):
+    manual_config = types.GenerateContentConfig(
+        tools=tools,
+        automatic_function_calling=types.AutomaticFunctionCallingConfig(disable=True),
+    )
+    chat = client.chats.create(model='gemini-3.6-flash',config = manual_config)
+    response = chat.send_message(user_request)
+
+    part = response.candidates[0].content.parts[0]
+    function_call = part.function_call
+
+    print(f"Gemini wannts to call: {function_call.name}")
+    print(f"with arguments: {function_call.args}")
+
+    available_functions = {
+        "create_reminder": create_reminder,
+        "list_reminders": list_reminders,
+    }
+    function_to_call = available_functions[function_call.name]
+    result = function_to_call(**function_call.args)
+
+    print(f"Execution result: {result}")
+    function_response_part = types.Part.from_function_response(
+        name=function_call.name,
+        response={"result": result},
+    )
+
+    final_response = chat.send_message(function_response_part)
+    print(f"Final reply: {final_response.text}")
+    
+
 
 if __name__ == "__main__":
-    inspect_raw_call("Remind me to buy milk at 6pm")
+    run_agent_manual("Remind me to buy milk at 6pm")
